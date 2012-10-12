df87a3da0846bf03f61768cd99760c6e6e9b0b9c
Artem <write2art@gmail.com>
2012-10-13 00:45:46 +0400

    Starting to refactor for zf2

---
+++ config/module.config.php
@@ -0,0 +1,118 @@
+<?php
+return array(
+    'controllers' => array(
+        'invokables' => array(
+            'generator' => 'Nexus\Controller\GeneratorController',
+        ),
+    ),
+    'service_manager' => array(
+        'aliases' => array(
+            'nexus_zend_db_adapter' => 'Zend\Db\Adapter\Adapter',
+        ),
+    ),
+    'console' => array(
+        'router' => array(
+            'routes' => array(
+                'nexus-generator' => array(
+                    'options' => array(
+                        'route' => 'nexus generator',
+                        'defaults' => array(
+                            'controller' => 'Nexus\Generator',
+                            'action' => 'index'
+                        )
+                    )
+                )
+            )
+        )
+    ),
+    'router' => array(
+        'routes' => array(
+            'zfcuser' => array(
+                'type' => 'Literal',
+                'priority' => 1000,
+                'options' => array(
+                    'route' => '/user',
+                    'defaults' => array(
+                        'controller' => 'zfcuser',
+                        'action'     => 'index',
+                    ),
+                ),
+                'may_terminate' => true,
+                'child_routes' => array(
+                    'login' => array(
+                        'type' => 'Literal',
+                        'options' => array(
+                            'route' => '/login',
+                            'defaults' => array(
+                                'controller' => 'zfcuser',
+                                'action'     => 'login',
+                            ),
+                        ),
+                    ),
+                    'authenticate' => array(
+                        'type' => 'Literal',
+                        'options' => array(
+                            'route' => '/authenticate',
+                            'defaults' => array(
+                                'controller' => 'zfcuser',
+                                'action'     => 'authenticate',
+                            ),
+                        ),
+                    ),
+                    'logout' => array(
+                        'type' => 'Literal',
+                        'options' => array(
+                            'route' => '/logout',
+                            'defaults' => array(
+                                'controller' => 'zfcuser',
+                                'action'     => 'logout',
+                            ),
+                        ),
+                    ),
+                    'register' => array(
+                        'type' => 'Literal',
+                        'options' => array(
+                            'route' => '/register',
+                            'defaults' => array(
+                                'controller' => 'zfcuser',
+                                'action'     => 'register',
+                            ),
+                        ),
+                    ),
+                    'changepassword' => array(
+                        'type' => 'Literal',
+                        'options' => array(
+                            'route' => '/change-password',
+                            'defaults' => array(
+                                'controller' => 'zfcuser',
+                                'action'     => 'changepassword',
+                            ),
+                        ),
+                        'may_terminate' => true,
+                        'child_routes' => array(
+                            'query' => array(
+                                'type' => 'Query',
+                            ),
+                        ),
+                    ),
+                    'changeemail' => array(
+                        'type' => 'Literal',
+                        'options' => array(
+                            'route' => '/change-email',
+                            'defaults' => array(
+                                'controller' => 'zfcuser',
+                                'action' => 'changeemail',
+                            ),
+                        ),
+                        'may_terminate' => true,
+                        'child_routes' => array(
+                            'query' => array(
+                                'type' => 'Query',
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        ),
+    ),
+);
